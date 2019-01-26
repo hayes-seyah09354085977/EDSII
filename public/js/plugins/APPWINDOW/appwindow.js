1e34fcfa79b0baa57f4aabc66f0143fed1730bfe
Charlie <crimmson17@gmail.com>
2019-01-26 13:22:25 +0800

    window

--- public/js/plugins/APPWINDOW/appwindow.js
+++ public/js/plugins/APPWINDOW/appwindow.js
@@ -1,4 +1,8 @@
-var activeapp = ''
+var activeapp = {
+    taskbarid: '',
+    appid: ''
+}
+
 var windowapp = {
     open: function(id, appname, content) {
         var appwindow = `
@@ -8,7 +12,7 @@ var windowapp = {
             <div class="column left">
             <span id="close" class="dot" style="background:#ED594A;"></span>
             <span class="dot" style="background:#FDD800;"></span>
-            <span class="dot" style="background:#5AC05A;"></span>
+            <span id="minimize" class="dot" style="background:#5AC05A;"></span>
             </div>
             <div class="column middle">
             ` + appname + `
@@ -26,32 +30,45 @@ var windowapp = {
            ` + content + `
         </div>
     </div>`
-        activeapp = id
-        $('.app-content').prepend(appwindow)
-        windowapp.taskbar(activeapp, appname, 'newapp')
-        windowapp.active(activeapp)
+        windowapp.ActiveApp(id)
+        if ($('#' + activeapp.appid).length == 0) {
+            $('.app-content').prepend(appwindow)
+            windowapp.taskbar(activeapp.taskbarid, appname, 'newapp') //Taskbar
+            windowapp.active(activeapp.appid)
 
-        $(".appcontainer").draggable({
-            containment: '.app-content',
-            handle: '.header'
-        });
-        console.log(activeapp)
-        $('#' + activeapp).find('#close').click(function() {
-            windowapp.close()
-            $('.taskbar').find('#' + activeapp).remove()
-        })
+            $(".appcontainer").draggable({
+                containment: '.app-content',
+                handle: '.header'
+            });
 
-        $('.appcontainer').click(function() {
-            activeapp = this.id
-            console.log(this)
-            windowapp.active(activeapp)
+            $('.appcontainer').click(function() {
+                windowapp.ActiveApp(this.id)
+                windowapp.active(activeapp.appid)
+                    //close button
+                var test = this.id
+                $('#' + test).find('#close').click(function() {
+                    windowapp.close(test)
+                    $('.taskbar').find('#' + test + '_bar').remove()
+                })
 
-        })
+            })
+            $("#minimize, .taskbar#" + activeapp.taskbarid).click(function() {
+                console.log($(this))
+                $("#" + activeapp.appid).animate({
+                    width: ["toggle", "swing"],
+                    height: ["toggle", "swing"],
+                    opacity: "toggle"
 
+                });
+            });
+        } else {
+            $('div#' + activeapp.appid).click()
+        }
 
     },
     close: function(id) {
-        $('#' + activeapp + '.appcontainer').remove()
+        console.log(id)
+        $('#' + id + '.appcontainer').remove()
         $('.taskbar').find('#' + id).remove()
     },
     active: function(id) {
@@ -62,30 +79,34 @@ var windowapp = {
         $('#' + id).addClass('activeapp')
         $('#' + id).find('.header').removeClass('form-inactive')
         $('#' + id).find('.header').addClass('form-active')
-        windowapp.taskbar(id)
+        windowapp.taskbar(activeapp.taskbarid)
     },
     taskbar: function(id, appname, response) {
+        console.log(id)
         var taskbarname = $('.taskbar #' + id),
             taskbar = $('.taskbar')
-        taskbar.find('.appbarInfo').removeClass('form-active')
-        $('.taskbar-name-default').addClass('taskbar-inactive')
-        taskbar.find('.appbarInfo').addClass('form-inactive')
 
-        switch (response) {
-            case 'newapp':
-                var apptaskbar = `
+
+        if (response == 'newapp') {
+            var apptaskbar = `
                 <div id="` + id + `" class="appbarInfo form-active">
                 <label class='taskbar-name-default'>` + appname + ` </label>
                 </div>`
-                taskbar.append(apptaskbar)
-                break;
+            taskbar.append(apptaskbar)
+        } else {
+            taskbar.find('.appbarInfo').removeClass('form-active')
+            $('.taskbar-name-default').addClass('taskbar-inactive')
+            taskbar.find('.appbarInfo').addClass('form-inactive')
 
-            default:
-                taskbarname.find('.taskbar-name-default').removeClass('taskbar-inactive')
-                taskbarname.removeClass('form-inactive')
-                taskbarname.addClass('form-active')
+            taskbarname.find('.taskbar-name-default').removeClass('taskbar-inactive')
+            taskbarname.removeClass('form-inactive')
+            taskbarname.addClass('form-active')
 
         }
+    },
+    ActiveApp: function(aid) {
+        activeapp.appid = aid
+        activeapp.taskbarid = aid + '_bar'
     }
 }
 
@@ -97,6 +118,3 @@ $('li.applist').click(function() {
     console.log(appid)
     windowapp.open(appid.split('_')[0], appname, appcontent)
 })
-
-// windowapp.open('PROBOOK', 'Pro Book')
-// windowapp.open('TEST2', 'Test2')
